9d48861f478aea58b7b3261a8bebea72b5ca8890
Christian Beretta <beretta.christian2602@gmail.com>
2020-02-19 15:19:07 +0100

    implementazione

--- js/main.js
+++ js/main.js
@@ -4,21 +4,23 @@
 // esprimere un messaggio in base al risultato precedente: se è nella lista= "benvenuto", altrimenti "non sei nella lista".
 
 //creazione array lista mail
-var listaEmail = ['frodo.baggins@hobbit.tdm', 'gandalf@mago.tdm', 'sam.gamgee@hobbit.tdm', 'sauron@mordor.tdm', 'gollum@smeagol.tdm']
+var listaEmail = ['frodo.baggins@hobbit.tdm', 'gandalf@mago.tdm', 'sam.gamgee@hobbit.tdm', 'legolas@elf.tdm', 'aragorn@granpasso.tdm']
 console.log(listaEmail);
 
+// chiedo all'utente di inserire il proprio indirizzo email
 var emailUtente = prompt('Inserisci il tuo indirizzo email completo');
 
+// ciclo for per confrontare email utente con email della lista: se presente, la mia variabile 'entra' assumerà valore 1, altrimenti 0.
 for (var i = 0; i < listaEmail.length; i++) {
     if (emailUtente === listaEmail[i]) {
         var entra = 1;
-    } else {
-        var entra = 0;
     }
 }
 
 if (entra === 1) {
-    console.log('Benvenuto');
+    document.getElementById('email-utente-in').innerHTML = emailUtente;
+    document.getElementsByClassName('benvenuto')[0].setAttribute('class','benvenuto visible');
 } else {
-    console.log('Arrivederci');
+    document.getElementById('email-utente-out').innerHTML = emailUtente;
+    document.getElementsByClassName('addio')[0].setAttribute('class','addio visible');
 }
